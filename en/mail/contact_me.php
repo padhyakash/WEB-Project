ecae0fa9049652220ab8ecb1684865e714152a2c
santoshlipu31 <santoshluckyy@gmail.com>
2018-04-03 13:05:05 +0530

    mail isuue
    mail issue resolve
    
    Co-Authored-By: Akash Padhy <padhyakash@gmail.com>

--- en/mail/contact_me.php
+++ en/mail/contact_me.php
@@ -1,11 +1,18 @@
 <?php
 
+$name = $_POST['name'];
+$phone = $_POST['phone'];
+$email = $_POST['email'];
+$message = $_POST['message'];
+
+
+
 // Create the email and send the message
 $to = 'padhyakash@gmail.com'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
 $email_subject = "Website Contact Form:  $name";
-$email_body = "You have received a new message from your website contact form.\n\n"."Here are the details:\n\nName: $name\n\nEmail: $email_address\n\nPhone: $phone\n\nMessage:\n$message";
+$email_body = "You have received a new message from your website contact form.\n\n"."Here are the details:\n\nName: $name\n\nEmail: $email\n\nPhone: $phone\n\nMessage:\n$message";
 $headers = "From: noreply@enableit.dk\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
-$headers .= "Reply-To: $email_address";	
+$headers .= "Reply-To: $email";	
 mail($to,$email_subject,$email_body,$headers);
 return true;			
 ?>
